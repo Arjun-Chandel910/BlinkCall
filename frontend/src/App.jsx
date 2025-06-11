bc40ce57d70d167e8b7a17e69a8ce4a3c19fb3a3
Arjun Chandel <arjunchandel910@gmail.com>
2025-06-11 20:07:40 +0530

    used framer motion and three js conponents to improve the styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,35 +7,43 @@ import { AuthProvider } from "./context/AuthContext";
 import VideoMeet from "./pages/VideoMeet";
 import MeetingCodePage from "./pages/MeetingCodePage";
 import VideoRoom from "./pages/VideoRoom";
-
+import ClickSpark from "./utils/ClickSpark";
 function App() {
   return (
     <div>
-      <Router>
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Bounce}
-        />
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <Router>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition={Bounce}
+          />
 
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="auth" element={<Authentication />} />
-            <Route path="/room/:url/prejoin" element={<VideoMeet />} />
-            <Route path="/room/:url" element={<VideoRoom />} />
-            <Route path="/roomcode" element={<MeetingCodePage />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="auth" element={<Authentication />} />
+              <Route path="/room/:url/prejoin" element={<VideoMeet />} />
+              <Route path="/room/:url" element={<VideoRoom />} />
+              <Route path="/roomcode" element={<MeetingCodePage />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </ClickSpark>
     </div>
   );
 }
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,58 +1,86 @@
 import React from "react";
 import phonesImage from "../utils/Group 77.png";
+import BlurText from "../utils/BlurText";
+import Beams from "../utils/Beams";
 import "../App.css";
 import { Link } from "react-router-dom";
+
 export default function LandingPage() {
   return (
-    <div className="landingPageContainer">
-      {/* navbar */}
-      <nav>
-        <div className="navHeader">
-          <h1>BlinkCall</h1>
-        </div>
-        <div className="navlist">
-          <h3>Join as Guest</h3>
-          <h3>Register</h3>
+    <div
+      className="landingPageContainer"
+      style={{ position: "relative", overflow: "hidden" }}
+    >
+      <div style={{ position: "absolute", inset: 0, zIndex: 0 }}>
+        <Beams
+          beamWidth={2}
+          beamHeight={15}
+          beamNumber={12}
+          lightColor="#ffffff"
+          speed={2}
+          noiseIntensity={1.75}
+          scale={0.2}
+          rotation={0}
+        />
+      </div>
 
-          <button>Login</button>
-        </div>
-      </nav>
+      <div style={{ position: "relative", zIndex: 10 }}>
+        {/* Navbar */}
+        <nav>
+          <div className="navHeader">
+            <h1>BlinkCall</h1>
+          </div>
+          <div className="navlist">
+            <h3>Join as Guest</h3>
+            <h3>Register</h3>
+            <button>Login</button>
+          </div>
+        </nav>
 
-      {/* body */}
-      <div className="landingPageBody">
-        <div className="landingPageLeft">
-          <h1>
-            <span style={{ color: "#FF8C00" }}>Connect </span>
-            with your loved Ones
-          </h1>
-          <p>
-            Cover a distance by{" "}
+        {/* Body */}
+        <div className="landingPageBody">
+          <div className="landingPageLeft">
+            <h1>
+              <span style={{ color: "#FF8C00" }}>Connect </span>
+              with your loved Ones
+            </h1>
             <span
               style={{
                 color: "#FF8C00",
                 opacity: "1",
               }}
             >
-              BlinkCall
+              <BlurText
+                text="Cover a distance with Blinkcall"
+                delay={150}
+                animateBy="words"
+                direction="top"
+                className="text-2xl mb-8"
+              />
             </span>
-          </p>
-          <div>
-            <Link
-              to={"/auth"}
-              style={{
-                color: "black",
-
-                textDecoration: "none",
-                paddingInline: "auto",
-                paddingBlock: "auto",
-              }}
-            >
-              Get Started
-            </Link>
+            <div>
+              <Link
+                to={"/auth"}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  paddingInline: "auto",
+                  paddingBlock: "auto",
+                }}
+              >
+                Get Started
+              </Link>
+            </div>
           </div>
-        </div>
-        <div>
-          <img src={phonesImage} alt="" />
+
+          {/* Right side image */}
+          <div>
+            <img
+              src={phonesImage}
+              alt="Phones"
+              style={{ maxWidth: "100%", height: "80%" }}
+            />
+          </div>
         </div>
       </div>
     </div>
